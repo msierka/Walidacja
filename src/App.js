ab8b47546558d3ba521ac681a48a157f5fde7fec
ms <mateusz@>
2023-11-16 16:26:30 +0100

    add mess

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ class App extends Component {
     name: '',
     email: '',
     password: '',
-    check: false
+    check: false,
+    error : {
+      name_error : true,
+      email_error : true,
+      password_error: true,
+      check_error: true,
+    }
    } 
+
+   mess = {
+    name_error : 'Login powinien mieć więcej niż 6 znaków oraz nie zawierać spacji',
+    email_error : 'Email musi zawierać znak @',
+    password_error: 'Hasło musi składać się z 6 znaków',
+    check_error: 'Niezaznaczona zgoda',
+   }
+
 
 handleSubmit=e=>{
   e.preventDefault();
@@ -22,31 +36,34 @@ handleChange=e=>{
     })
   }else if(name === 'check'){
     const value = e.target.checked;
-    console.log(value)
     this.setState({
       check: value
     })
   }
-  //console.log(type,name)
 
 }
 
 
   render() { 
+    const {name_error, email_error, password_error, check_error} = this.mess;
     return (
       <form onSubmit={this.handleSubmit}>
         <label htmlFor="text"> Twój login
           <input type="text" id="text" name="name" value={this.state.name} onChange={this.handleChange}/>
+        {this.state.error.name_error && <span>{name_error}</span>}
          </label> 
          <label htmlFor="email"> Twój email
           <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange}/>
+          {this.state.error.email_error && <span>{email_error}</span>}
          </label> 
          <label htmlFor="password"> Twoje hasło
           <input type="password" id="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+          {this.state.error.password_error && <span>{password_error}</span>}
          </label> 
          <label htmlFor="checkbox"> 
           <input type="checkbox" id="checkbox" name="check" value={this.state.check} onChange={this.handleChange}/> Wyrażam zgodę
          </label> 
+         {this.state.error.check_error && <span>{check_error}</span>}
          <button>Zapisz się</button>
          
       </form>
